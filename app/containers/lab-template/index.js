5711e30d531ee5aaf12b33c0db2f149582055123
Apollo Tang <tang.apollo@gmail.com>
2017-06-10 00:04:02 -0400

    create template

---
+++ app/containers/lab-template/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Animated, SectionList, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationActions } from 'react-navigation';
+import { func, number, bool, string, object, arrayOf } from 'prop-types';
+
+import navIndex from '../../common/navIndex';
+import CollectionItem from '../../components/collectionItem/';
+import Spinner from '../../components/spinner/';
+import Card from '../../components/card/';
+
+import { mapStoreToProps, mapDispatchToProps } from './selector';
+
+const Action_navigateToRoute = (routeName, params) => {
+  return NavigationActions.navigate({ routeName, params });
+};
+
+@connect(mapStoreToProps, mapDispatchToProps)
+class Screen_Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+  }
+
+  componentDidUpdate(prevProps) {
+  }
+
+  render() {
+    console.log('xxxxxx this.props: ', this.props)
+    return (
+      <View>
+        <Text>
+          hello world
+        </Text>
+      </View>
+    );
+  }
+}
+
+Screen_Dashboard.defaultProps = {
+};
+
+// export default connect(mapStoreToProps, mapDispatchToProps)(Screen_Dashboard);
+export default Screen_Dashboard;
